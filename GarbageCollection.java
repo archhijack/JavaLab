c316164e9e2bb632e48560502a8dad9209f244c8
Archisman Bhattacharya <archhijack@gmail.com>
2020-08-11 11:28:49 +0530

    Java Lab Program 1 and 2

--- GarbageCollection.java
+++ GarbageCollection.java
@@ -1,17 +1,19 @@
 public class GarbageCollection {										//Class
-	public void Bin(boolean stat) { 									//Member Function, Access Specifier
+	public void Bin(boolean stat, int num) {				 			//Member Function, Access Specifier
+		System.out.print("Bin "+num+" "); 								
     	if(stat==true){													//Data Member, Control Statement
-    		System.out.println("The Bin is full.");
+    		System.out.print("is full.");
     	}
     	else
-    		System.out.println("The Bin is empty.");
+    		System.out.print("is empty.");
+    	System.out.println();
   	}
 
   	public static void main(String[] args) {		 					//Main
   		boolean[] binStat = {true, false, true, true, false};			//Array
     	GarbageCollection bins = new GarbageCollection();				//Object of class to call non-static function
     	for(int i=0; i<binStat.length; i++){							//Loop
-    		bins.Bin(binStat[i]);										//Calling Function
+    		bins.Bin(binStat[i], i+1);									//Calling Function
     	}
   	}
 }
